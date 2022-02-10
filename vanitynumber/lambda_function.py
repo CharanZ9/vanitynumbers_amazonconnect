49d0682e8e9d05ec1e42c8facafe2953c23348ee
CharanZ9 <saicharan.atthineni@gmail.com>
2022-02-10 18:13:35 +0000

    VF-Proj: Updates to parse the country code

--- vanitynumber/lambda_function.py
+++ vanitynumber/lambda_function.py
@@ -8,38 +8,55 @@ import boto3
 
 def lambda_handler(event, context):
     phone_number = ''
-    if("NAME" in event.keys()):
-        if event["NAME"]=='ContactFlowEvent':
-            phone_number = event["Details"]["ContactData"]["CustomerEndpoint"]["Address"] 
-            vanity_number = get_vanity_number(phone_number)
-            result_map = {"VanityNumber1":str(vanity_number[0]),"VanityNumber2":str(vanity_number[1]),"VanityNumber3":str(vanity_number[2])}
+    print(event)
+    if ("Name" in event.keys()):
+        if event["Name"] == 'ContactFlowEvent':
+            phone_number = event["Details"]["ContactData"]["CustomerEndpoint"]["Address"]
+            if ('+44' in phone_number):
+                phone_number = str(phone_number)[3:]
+                vanity_number = get_vanity_number(phone_number)
+                for i in range(0, len(vanity_number)):
+                    vanity_number[i] = '+44' + str(vanity_number[i])
+            else:
+                vanity_number = get_vanity_number(phone_number)
+            result_map = {"VanityNumber1": str(vanity_number[0]), "VanityNumber2": str(vanity_number[1]),
+                          "VanityNumber3": str(vanity_number[2])}
             return result_map
-    else: 
+    else:
         number = event["queryStringParameters"]['phonenumber']
-        vanity_number = get_vanity_number(number)
-        result_map = {"VanityNumber1":str(vanity_number[0]),"VanityNumber2":str(vanity_number[1]),"VanityNumber3":str(vanity_number[2])}
+        if ('+44' in number):
+            number = str(number)[3:]
+            vanity_number = get_vanity_number(number)
+            for i in range(0, len(vanity_number)):
+                vanity_number[i] = '+44' + str(vanity_number[i])
+        else:
+            vanity_number = get_vanity_number(number)
+        result_map = {"VanityNumber1": str(vanity_number[0]), "VanityNumber2": str(vanity_number[1]),
+                      "VanityNumber3": str(vanity_number[2])}
         return {
-        'statusCode' : 200,
-        'body' : json.dumps(result_map)
+            'statusCode': 200,
+            'body': json.dumps(result_map)
 
         }
 
+
 def get_vanity_number(phone_number):
     vanity_number_from_db = get_item(phone_number)
-    if('item' in vanity_number_from_db.keys()):
+    if ('item' in vanity_number_from_db.keys()):
         return vanity_number_from_db['item']['vanity_number']
-    else: 
+    else:
         vanity_number = wordify.all_wordifications(phone_number)
-        put_response = put_item(phone_number,vanity_number)
+        put_response = put_item(phone_number, vanity_number)
         return vanity_number
-        
+
+
 def put_item(phone_number, vanity_number, dynamodb=None):
     if not dynamodb:
         dynamodb = boto3.resource('dynamodb')
 
     table = dynamodb.Table('vanitynumbertable')
     response = table.put_item(
-       Item={
+        Item={
             'phoneNumber': phone_number,
             'vanityNumber': vanity_number,
         }
